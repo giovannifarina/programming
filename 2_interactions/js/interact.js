b466a1ee3b7c41e96927ea91ad6ad8a3569708fd
giovannifarina <47982887+giovannifarina@users.noreply.github.com>
2023-03-01 01:30:19 +0100

    first upload

---
+++ 2_interactions/js/interact.js
@@ -0,0 +1,7 @@
+alert("Hello again!");
+let risposta = prompt('Digita qualcosa!');
+console.log(risposta);
+risposta = prompt('Digita qualcosa!','Ciao?');
+console.log(risposta);
+risposta = confirm('Sei confuso/a?');
+console.log(risposta);
