2a45d61b5f79502fad6f5895d71fb76a75121af7
giovannifarina <47982887+giovannifarina@users.noreply.github.com>
2023-04-26 07:37:40 +0200

    update

--- 9_DOM/navigate_dom/code.js
+++ 9_DOM/navigate_dom/code.js
@@ -4,3 +4,12 @@ let listItems = listaSpesa.querySelectorAll('li');
 for (var listItem of listItems) {
     listItem.setAttribute('style', 'font-weight: bold;');
 }
+
+let paragraph = document.createElement('p');
+paragraph.textContent = 'Questo è un nuovo paragrafo aggiunto con JavaScript!';
+
+// Append the paragraph element to the body
+document.body.appendChild(paragraph);
+
+let paragrafo = "<p>Altro Paragrafo!</p>";
+document.querySelector("div").innerHTML = paragrafo;
