9b6d81bb11b8dc2dcfb4a275c461af3cf83350b6
giovannifarina <47982887+giovannifarina@users.noreply.github.com>
2023-04-26 06:30:30 +0200

    added example

---
+++ 9_DOM/button_event/code.js
@@ -0,0 +1,15 @@
+let button = document.querySelector('.myButton');
+
+// Add click event listener
+/*
+button.addEventListener('click', function() {
+    alert('Hai cliccato il bottone!');
+});
+*/
+
+button.addEventListener("click", reagisciClickBottone);
+
+
+function reagisciClickBottone() {
+    alert('Hai cliccato il bottone!');
+}
