0c10b04ef2a2dc9e568e3d16dd88513ecafd12c2
giovannifarina <47982887+giovannifarina@users.noreply.github.com>
2023-04-26 10:01:27 +0200

    update

--- 9_DOM/3_addElement2/code.js
+++ 9_DOM/3_addElement2/code.js
@@ -6,6 +6,8 @@ function createNewElement() {
     newElement.id = 'myNewElement' + idCounter.toString();
     idCounter++;
     newElement.className = 'my-custom-class';
+    //  newElement.classList.add('my-custom-class', 'another-class', 'third-class'); // per aggiungere più classi
+
 
     // Set inner text for the new element
     newElement.innerText = 'This is a dynamically created element!';
