8d9e7ea4e4ec3e17f7fad3d0aee58b1e5b3f969e
giovannifarina <47982887+giovannifarina@users.noreply.github.com>
2023-04-26 09:49:33 +0200

    added example

---
+++ 9_DOM/addElement2/code.js
@@ -0,0 +1,19 @@
+function createNewElement() {
+    // Create a new <div> element
+    var newElement = document.createElement('div');
+
+    // Set id and class attributes for the new element
+    newElement.id = 'myNewElement' + idCounter.toString();
+    idCounter++;
+    newElement.className = 'my-custom-class';
+
+    // Set inner text for the new element
+    newElement.innerText = 'This is a dynamically created element!';
+
+    // Append the new element to the DOM
+    document.body.appendChild(newElement);
+  }
+
+let idCounter = 1
+
+document.getElementById('bottone').addEventListener('click', createNewElement);
